ed8fe4d495187f3bf1c6ea9002b13042dcc28aaf
devsysy <paeksoyeon1@gmail.com>
2021-03-15 12:03:24 +0900

    .

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,11 +4,16 @@
       <li v-for="(todoItem, i) in $store.state.todoItems" v-bind:key="i" ref="chkValue2">
         <input type="checkbox"
                ref="chkValue"
-               @click="chkBtn(todoItem, i)">
+               v-bind:class="chkOn = !chkOn"
+               @click="toggleBtn(lineThroughOn = !lineThroughOn)"
+               >
         {{i+1+'.'}}
-        {{todoItem}}
-        <button type="text"
-                @click="removeTodo(todoItem, i)">remove</button>
+        <span v-bind:class="todoItemClasses">{{todoItem}}</span>
+        <button type="button"
+                @click="removeTodo(todoItem, i)">Remove</button>
+        <button type="button">완료</button>
+        <button type="button">미완료</button>
+
       </li>
     </ul>
   </section>
@@ -19,23 +24,38 @@
     name: "TodoList",
     data(){
       return{
+        lineThroughOn: true,
+        chkOnchk: 'checked'
+      }
+    },
+    computed: {
+      todoItemChk: function(){
+        
+      }
+    },
+    computed: {
+      todoItemClasses: function(){
+        return{
+          lineOn: this.lineThroughOn,
+          lineOff: !this.lineThroughOn,
+        }
       }
     },
     methods: {
       removeTodo: function(todoItem, i){
         this.$store.commit('removeTodo',{todoItem, i});
       },
-      chkBtn(todoItem, i){
+      /*toggleBtn(i){
         if(this.$refs.chkValue[i].checked === true){
-          this.$refs.chkValue2[i].style.textDecoration="line-through";
-        }else{
-          this.$refs.chkValue2[i].style.textDecoration="none";
+          //this.$refs.chkValue2[i].style.textDecoration="line-through";
+          console.log('fdsfsd');
         }
-      },
+      },*/
     }
   }
 </script>
 
 <style scoped>
   li{list-style: none;}
+  .lineThroughOn{ text-decoration: line-through; }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {store} from './store'
+import VueMoment from 'vue-moment'
 
 Vue.config.productionTip = false
+Vue.use(VueMoment)
+
 
 new Vue({
   store: store,
