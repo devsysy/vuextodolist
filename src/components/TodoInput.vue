45c18b9b025e46c4af232323ecdcb9d64c3b17d2
devsysy <paeksoyeon1@gmail.com>
2021-03-15 20:15:13 +0900

    2.

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -17,9 +17,13 @@
       }
     },
     methods: {
-      addTodo: function(n){
+      addTodo: function(n){//Todo 생성할 때, 즉 버튼이 눌리는 시점이 작성일시가 되는 것이기 때문에!
+        //Todo 생성할 때, vue-moment 라이브러리에서 날짜 date 호출하여, today 상수에 담기
+        const today = this.$moment().format('YYYY-MM-DD hh:mm:ss');
+
         if(this.newTodoItem.trim() !== '' ){
-          this.$store.state.todoItems.push(this.newTodoItem)
+          //Todo 생성할 때, 날짜 date를 호출해야지만, 화면 렌더링시 날짜 data update 되지 않음
+          this.$store.state.todoItems.push(this.newTodoItem + '작성일자:' + today)
           n.preventDefault()
         }else{
           alert('Nothing to do today?!')
